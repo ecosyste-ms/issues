5f026d27bcec6bafadaaad137fa287579edcb9c2
Andrew Nesbitt <andrewnez@gmail.com>
2023-05-10 12:42:45 +0100

    paginate correctly in gitlab api requests

--- app/models/hosts/gitlab.rb
+++ app/models/hosts/gitlab.rb
@@ -13,58 +13,58 @@ module Hosts
     def load_issues(repository)
       options = {state: 'all', order_by: 'updated_at', sort: 'desc', per_page: 100}
       options[:updated_after] = repository.last_synced_at if repository.last_synced_at.present?
-      # TODO pagination
+      
       issues = api_client.issues(repository.full_name, options)
-      mapped_issues = issues.map do |issue|
-        {
-          uuid: issue.id,
-          number: issue.iid,
-          title: issue.title,
-          state: issue.state,
-          locked: issue.discussion_locked,
-          comments_count: issue.user_notes_count,
-          created_at: issue.created_at,
-          updated_at: issue.updated_at,
-          closed_at: issue.closed_at,
-          user: issue.author.username,
-          labels: issue.labels,
-          assignees: issue.assignees.map(&:username),
-          pull_request: false,
-        }
+      while issues.next_page.present?
+        mapped_issues = issues.map do |issue|
+          {
+            uuid: issue.id,
+            number: issue.iid,
+            title: issue.title,
+            state: issue.state,
+            locked: issue.discussion_locked,
+            comments_count: issue.user_notes_count,
+            created_at: issue.created_at,
+            updated_at: issue.updated_at,
+            closed_at: issue.closed_at,
+            user: issue.author.username,
+            labels: issue.labels,
+            assignees: issue.assignees.map(&:username),
+            pull_request: false,
+          }
+        end
+
+        yield mapped_issues 
+        issues = issues.next_page
       end
 
-      yield  (mapped_issues += load_merge_requests(repository))
-    end
-
-    def load_merge_requests(repository)
-      options = {state: 'all', order_by: 'updated_at', sort: 'desc', per_page: 100}
-      options[:updated_after] = repository.last_synced_at if repository.last_synced_at.present?
-      # TODO pagination
       merge_requests = api_client.merge_requests(repository.full_name, options)
-      merge_requests.map do |merge_request|
-        {
-          uuid: merge_request.id,
-          number: merge_request.iid,
-          title: merge_request.title,
-          state: merge_request.state,
-          locked: merge_request.discussion_locked,
-          comments_count: merge_request.user_notes_count,
-          created_at: merge_request.created_at,
-          updated_at: merge_request.updated_at,
-          closed_at: merge_request.closed_at,
-          user: merge_request.author.username,
-          labels: merge_request.labels,
-          assignees: merge_request.assignees.map(&:username),
-          pull_request: true,
-        }
+      while merge_requests.next_page.present?
+        mapped_merge_requests = merge_requests.map do |merge_request|
+          {
+            uuid: merge_request.id,
+            number: merge_request.iid,
+            title: merge_request.title,
+            state: merge_request.state,
+            locked: merge_request.discussion_locked,
+            comments_count: merge_request.user_notes_count,
+            created_at: merge_request.created_at,
+            updated_at: merge_request.updated_at,
+            closed_at: merge_request.closed_at,
+            user: merge_request.author.username,
+            labels: merge_request.labels,
+            assignees: merge_request.assignees.map(&:username),
+            pull_request: true,
+          }
+        end
+
+        yield mapped_merge_requests
+        merge_requests = merge_requests.next_page
       end
     end
 
     def api_client
       ::Gitlab.client(endpoint: "#{@host.url}/api/v4", private_token:  REDIS.get("gitlab_token:#{@host.id}"))
     end
-
-
-
   end
 end
