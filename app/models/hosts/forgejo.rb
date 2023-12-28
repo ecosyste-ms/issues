d3cdfd57b03c659b196913f83de509c7d357efdd
Andrew Nesbitt <andrewnez@gmail.com>
2023-12-28 19:38:21 +0000

    Add Forgejo host class #190

---
+++ app/models/hosts/forgejo.rb
@@ -0,0 +1,14 @@
+module Hosts
+  class Forgejo < Gitea
+    def icon
+      'forgejo'
+    end
+
+    def api_client
+      Faraday.new(@host.url, request: {timeout: 30}) do |conn|
+        conn.request :authorization, :bearer, REDIS.get("forgejo_token:#{@host.id}")
+        conn.response :json        
+      end
+    end
+  end
+end
