1b637724d3aa14cbdc9e0a5e81e474435fc6d7be
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-09 14:12:55 +0100

    Add conditional index creation for issues table to prevent duplication

--- db/migrate/20250707184305_add_index_to_issues_on_host_id_and_uuid.rb
+++ db/migrate/20250707184305_add_index_to_issues_on_host_id_and_uuid.rb
@@ -2,6 +2,8 @@ class AddIndexToIssuesOnHostIdAndUuid < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_index :issues, [:host_id, :uuid], algorithm: :concurrently, unique: true, name: 'index_issues_on_host_id_and_uuid'
+    unless index_exists?(:issues, [:host_id, :uuid], name: 'index_issues_on_host_id_and_uuid')
+      add_index :issues, [:host_id, :uuid], algorithm: :concurrently, unique: true, name: 'index_issues_on_host_id_and_uuid'
+    end
   end
 end
