d843a67c05ec88079edebfbd4555509f0acd807c
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-10 09:25:14 +0100

    tweak migration

--- db/migrate/20250707184305_add_index_to_issues_on_host_id_and_uuid.rb
+++ db/migrate/20250707184305_add_index_to_issues_on_host_id_and_uuid.rb
@@ -1,9 +1,6 @@
 class AddIndexToIssuesOnHostIdAndUuid < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
 
   def change
-    unless index_exists?(:issues, [:host_id, :uuid], name: 'index_issues_on_host_id_and_uuid')
-      add_index :issues, [:host_id, :uuid], algorithm: :concurrently, unique: true, name: 'index_issues_on_host_id_and_uuid'
-    end
+    add_index :issues, [:host_id, :uuid], unique: true, name: 'index_issues_on_host_id_and_uuid'
   end
 end
